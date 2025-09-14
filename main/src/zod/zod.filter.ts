284d2912f579e0e7d04e1b84d2d7eb8c11ee43a1
andre-hctulc <andre.tho.44@gmail.com>
2025-09-15 01:30:41 +0200

    >>

--- main/src/zod/zod.filter.ts
+++ main/src/zod/zod.filter.ts
@@ -1,9 +1,25 @@
 import { ArgumentsHost, Catch, ExceptionFilter } from "@nestjs/common";
 import { ZodError } from "zod";
-import { ErrorBody } from "../common/index.js";
+import { ErrorBody, LogLevel } from "../common/index.js";
+import { log } from "../common/util/system/system-util.js";
+
+interface ZodExceptionFilterOptions {
+    /**
+     * "verbose": Log all exceptions
+     *
+     * "error" | "info": Log only unmapped and non rpc errors
+     */
+    logLevel?: LogLevel;
+}
 
 @Catch(ZodError)
 export class ZodExceptionFilter implements ExceptionFilter {
+    private logLevel: LogLevel;
+
+    constructor(private options?: ZodExceptionFilterOptions) {
+        this.logLevel = options?.logLevel || "verbose";
+    }
+
     async catch(exception: ZodError, host: ArgumentsHost) {
         const ctxType = host.getType();
         const errorBody: ErrorBody = {
@@ -13,6 +29,8 @@ export class ZodExceptionFilter implements ExceptionFilter {
             },
             status: 400,
         };
+
+        log(this.logLevel, "error", exception);
 
         if (ctxType === "http") {
             const http = host.switchToHttp();
