5ee150524768698017f8ce87670811071f6580f9
andre-hctulc <andre.tho.44@gmail.com>
2025-10-21 01:36:10 +0200

    >>

--- main/src/common/decorators/access.decorator.ts
+++ main/src/common/decorators/access.decorator.ts
@@ -1,4 +1,4 @@
-import { createParamDecorator } from "@nestjs/common";
+import { createParamDecorator, UnauthorizedException } from "@nestjs/common";
 import type { FastifyRequest } from "fastify";
 import { APIAccess } from "../util/api-access.class.js";
 
@@ -7,10 +7,34 @@ type APIAccessConstructor = abstract new (...args: any) => APIAccess;
 // TODO support different context
 /**
  * Decorator to confirm API access. Checks `req.apiAccess` against the provided `AccessClass`.
- * @param AccessClass - The class to confirm the API access against. If not provided, the base APIAccess class is used.
+ * @param AccessClass `ApiAccess` classes to confirm against. If multiple classes are provided, access is granted if any match.
  */
-export const Access = createParamDecorator<APIAccessConstructor, APIAccess>((AccessClass, ctx) => {
+export const Access = createParamDecorator<
+    APIAccessConstructor | APIAccessConstructor[] | undefined | null | never,
+    APIAccess
+>((AccessClass, ctx) => {
     const http = ctx.switchToHttp();
     const req: FastifyRequest = http.getRequest();
+
+    if (Array.isArray(AccessClass)) {
+        let someAccess: APIAccess | null = null;
+
+        for (const Access of AccessClass) {
+            try {
+                const access = APIAccess.confirm(req.apiAccess, Access);
+                if (access) {
+                    someAccess = access;
+                    break;
+                }
+            } catch (e) {}
+        }
+
+        if (!someAccess) {
+            throw new UnauthorizedException();
+        }
+
+        return someAccess;
+    }
+
     return APIAccess.confirm(req.apiAccess, AccessClass || APIAccess);
 });
